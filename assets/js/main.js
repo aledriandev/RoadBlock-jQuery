5d5c4e93f79e6d660c9e8ab4166fc6343123f28e
AQP20171679 <alejandraadriantejada@gmail.com>
2017-08-29 08:17:34 -0500

    Agregando un poco de jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
-/****** FUNCIONALIDAD GAME ******/
-var start = document.getElementById('start-button');
-var menu = document.getElementById('menu');
-var level_1 = document.getElementById('level-1');
-var tableros = document.createElement('div');
-var x; //Posicion actual x
-var y; //Posicion actual y
-var xTrans = []; //Posicion x de trasportacion
-var yTrans = []; //Posicion y de transportacion
-var transp;
-var filas; // filas del mapa
-var columnas; // columnas del mapa
-var arrayMapa = [];
-var tabla;
-var level = 0; //contador de niveles, para que cambie de mapa
-
-level_1.appendChild(tableros);
-
 var map1 = [" * * * * * * * * * * * * * * * * * * * * ",
     " * *           * * *             * *   * ",
     " * *             W                 *   * ",
@@ -164,65 +146,85 @@ var map6 = [  "                                                 ",
 
 var mapas = [map1, map2, map3, map4, map5, map6]; //contenedor de mapas
 var mapa = [];
+var tableros;
+var x; //Posicion actual x
+var y; //Posicion actual y
+var xTrans = []; //Posicion x de trasportacion
+var yTrans = []; //Posicion y de transportacion
+var transp;
+var filas; // filas del mapa
+var columnas; // columnas del mapa
+var arrayMapa = [];
+var tabla;
+var level = 0; //contador de niveles, para que cambie de mapa
 
 var chuckGif;
-function generaMapa(){
-  mapa = mapas[level];
-  for (var i = 0; i < mapa.length; i++) {
-	   for (var j = 0; j < mapa[i].length; j++) {
-	      var M= mapa[i].split("");
-	   }
-	   arrayMapa.push(M);
-	}
-	filas = mapa.length;
-	columnas = mapa[0].length;
 
-	tabla = document.createElement('table');
-	tabla.border = "0";
-	for (var i = 0; i < filas; i++) {
-	   var fila = document.createElement('tr');
-	   for (var j = 0; j < columnas; j++) {
-	     var celda = document.createElement('td');
-	      if (mapa[i][j] == "*") {
-	      celda.setAttribute('class', 'pared');
-            var imgPared = document.createElement('img');
-                imgPared.src = "assets/img/tnt.png"
-            celda.appendChild(imgPared);
-	     } else if(mapa[i][j] == "o") {
-	      celda.setAttribute('class','inicio')
-        chuckGif = document.createElement('img');
-            chuckGif.setAttribute("id",'img-chuck')
-        chuckGif.src = "assets/img/angry.gif"
-        celda.appendChild(chuckGif);
-	      x = i;
-	      y = j;
-	   } else if(mapa[i][j] == "W") {
-	      celda.setAttribute('class','final')
-        nest = document.createElement('img');
-            nest.setAttribute("id",'img-nest')
-        nest.src = "assets/img/nido.png"
-        celda.appendChild(nest);
-	   } else if(mapa[i][j] == "I") {
-	      celda.setAttribute('class','transporta');
-          xTrans.push(i);
-          yTrans.push(j);
-	   }else{
-	      celda.setAttribute('class','blanco');
-	   }
-	   fila.appendChild(celda);
-	   arrayMapa[i][j] = celda;
-	  }
-	  tabla.appendChild(fila);
-	}
-	tableros.appendChild(tabla);
-}
+/****** FUNCIONALIDAD GAME ******/
+$(document).ready(function(){
+    $('h2').text("Este codigo esta con jQuery")
+    tableros = $('<div>');
+    $('#level-1').append(tableros);   
+    var generaMapa = function(){
+        mapa = mapas[level];
+        for (var i = 0; i < mapa.length; i++) {
+           for (var j = 0; j < mapa[i].length; j++) {
+              var M= mapa[i].split("");
+           }
+           arrayMapa.push(M);
+        }
+        filas = mapa.length;
+        columnas = mapa[0].length;
 
-start.addEventListener('click',play);
+        tabla = $('<table>');
+        tabla.border = "0";
+        for (var i = 0; i < filas; i++) {
+            var fila = document.createElement('tr');
+            for (var j = 0; j < columnas; j++) {
+                var celda = document.createElement('td');
+                if (mapa[i][j] == "*") {
+                    celda.setAttribute('class', 'pared');
+                    var imgPared = document.createElement('img');
+                        imgPared.src = "assets/img/tnt.png"
+                    celda.appendChild(imgPared);
+                } else if(mapa[i][j] == "o") {
+                    celda.setAttribute('class','inicio')
+                    chuckGif = document.createElement('img');
+                        chuckGif.setAttribute("id",'img-chuck')
+                        chuckGif.src = "assets/img/angry.gif"
+                    celda.appendChild(chuckGif);
+                    x = i;
+                    y = j;
+            } else if(mapa[i][j] == "W") {
+                celda.setAttribute('class','final')
+                nest = document.createElement('img');
+                    nest.setAttribute("id",'img-nest')
+                    nest.src = "assets/img/nido.png"
+                celda.appendChild(nest);
+           } else if(mapa[i][j] == "I") {
+                celda.setAttribute('class','transporta');
+                xTrans.push(i);
+                yTrans.push(j);
+           }else{
+                celda.setAttribute('class','blanco');
+           }
+           fila.appendChild(celda);
+           arrayMapa[i][j] = celda;
+          }
+          tabla.append(fila);
+        }
+        tableros.append(tabla);
+    }
 
-function play(){
-	menu.style.display = "none";
-	generaMapa();
-}
+    $('#start-button').click(function(){
+        $('#menu').css({'display':'none'});
+        generaMapa()
+    });
+
+
+});
+
+
 
 
 //keycode de las teclas
